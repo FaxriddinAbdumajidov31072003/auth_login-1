4346d2945ab21f3db8fc42690b3a497217e725b1
jamshidmirzamaxmudov <jamshimirzamxmudov3@gmail.com>
2023-01-25 16:19:02 +0500

    -set up fireStore

--- lib/add_number.dart
+++ lib/add_number.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
 import 'package:auth_login/verfy_code.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AddNumber extends StatefulWidget {
   const AddNumber({Key? key}) : super(key: key);
@@ -12,7 +16,7 @@ class AddNumber extends StatefulWidget {
 
 class _AddNumberState extends State<AddNumber> {
   final TextEditingController controller = TextEditingController();
-
+  String imageUrl = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,24 +30,34 @@ class _AddNumberState extends State<AddNumber> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-        var data = await  FirebaseFirestore.instance.collection('users').add({
-            'sender': {"id": 6, "name": "Mupza"}, // John Doe
-            'resender': {"id": 3, "name": "Test3"}, // Stokes and Sons
+          final ImagePicker _picker = ImagePicker();
+          // Pick an image
+          final XFile? image =
+              await _picker.pickImage(source: ImageSource.gallery);
+          try {
+            final storageRef = FirebaseStorage.instance.ref().child("file/");
+            var res =  await storageRef.putFile(File(image?.path ?? ""));
+            imageUrl = await res.ref.getDownloadURL();
+          } on FirebaseException catch (e) {
+            print(e);
+          }
+          var data = await FirebaseFirestore.instance.collection('users').add({
+
+            'sender': {"id": 6, "name": "Mupza","image" : imageUrl}, // John Doe
+            'resender': {"id": 3, "name": "Test3","image" : imageUrl}, // Stokes and Sons
             'ids': [6, 3] // 42
           });
 
-        FirebaseFirestore.instance.collection('users').doc(data.id).collection("chats").add({
-          "messages":[
-            {
-              "id" : 6,
-              "text" : "New Message"
-            },
-            {
-              "id" : 3,
-              "text" : "Last Message"
-            },
-          ]
-        });
+          FirebaseFirestore.instance
+              .collection('users')
+              .doc(data.id)
+              .collection("chats")
+              .add({
+            "messages": [
+              {"id": 6, "text": "New Message"},
+              {"id": 3, "text": "Last Message"},
+            ]
+          });
 
           // await FirebaseAuth.instance.verifyPhoneNumber(
           //   phoneNumber: controller.text,
