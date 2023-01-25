85bda7c6c1df3516766cf180923619d869ec1f12
jamshidmirzamaxmudov <jamshimirzamxmudov3@gmail.com>
2023-01-25 17:25:53 +0500

    -set up fireStore

--- lib/get_product.dart
+++ lib/get_product.dart
@@ -14,6 +14,33 @@ class GetProductPage extends StatefulWidget {
 class _GetProductPageState extends State<GetProductPage> {
   final FirebaseFirestore fireStore = FirebaseFirestore.instance;
   List<ProductModel> list = [];
+  QuerySnapshot? data;
+  bool isLoading = true;
+
+  Future<void> getInfo({String? text}) async {
+    isLoading = true;
+    setState(() {});
+    if(text == null){
+      data = await fireStore.collection("product").get();
+    }else{
+      data = await fireStore
+          .collection("product")
+          .orderBy("name")
+          .startAt([text]).endAt(["$text\uf8ff"]).get();
+    }
+    list.clear();
+    data?.docs.forEach((element) {
+      list.add(ProductModel.fromJson(element));
+    });
+    isLoading = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getInfo();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,42 +48,43 @@ class _GetProductPageState extends State<GetProductPage> {
       appBar: AppBar(
         title: Text('Products'),
       ),
-      body: FutureBuilder(
-        future: fireStore.collection("product").where("name",isEqualTo: "mac").get(),
-        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasData) {
-            list.clear();
-            snapshot.data?.docs.forEach((element) {
-              list.add(ProductModel.fromJson(element));
-            });
-            return ListView.builder(
-                itemCount: list.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    margin: EdgeInsets.only(bottom: 10),
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                    ),
-                    child: Column(
-                      children: [
-                        Text(list[index].name),
-                        Text(list[index].desc),
-                        Text(list[index].price.toString()),
-                      ],
-                    ),
-                  );
-                });
-          } else if (snapshot.hasError) {
-            return Center(child: Text(snapshot.error.toString()));
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              decoration: InputDecoration(labelText: "Search"),
+              onChanged: (s){
+                getInfo(text: s);
+              },
+            ),
+          ),
+          isLoading
+              ? CircularProgressIndicator()
+              : ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: list.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.only(bottom: 10),
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                      ),
+                      child: Column(
+                        children: [
+                          Text(list[index].name),
+                          Text(list[index].desc),
+                          Text(list[index].price.toString()),
+                        ],
+                      ),
+                    );
+                  })
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (_) => const AddProductPage()));
+          Navigator.push(context,
+              MaterialPageRoute(builder: (_) => const AddProductPage()));
         },
         child: const Icon(Icons.add),
       ),
